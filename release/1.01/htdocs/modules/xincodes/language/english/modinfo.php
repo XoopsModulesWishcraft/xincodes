c05bd65729edcf76f3cd12cdba28352aadb321b5
wishcraft <wishcraft@c17157d5-08f2-41e7-a131-df055f6a0da3>
2010-12-10 21:56:39 +0000

    XinCodes 1.01 - Invite Codes for Profile 1.62/1.61

---
+++ release/1.01/htdocs/modules/xincodes/language/english/modinfo.php
@@ -0,0 +1,12 @@
+<?php
+
+	define('_XIN_CREATECODE', 'Create Invite Code');
+	
+	// Preferences
+	define('_XIN_NUMBEROFCODES', 'Number of Codes');
+	define('_XIN_NUMBEROFCODES_DESC', 'This is the number of codes each user can create.');
+	define('_XIN_CODEPARTS', 'Parts to make code from');
+	define('_XIN_CODEPARTS_DESC', 'This is the parts used to create codes, they are seperated with a pipe (|).');
+	define('_XIN_CODELEN', 'Length of code');
+	define('_XIN_CODELEN_DESC', 'This is the length of the code.');
+?>

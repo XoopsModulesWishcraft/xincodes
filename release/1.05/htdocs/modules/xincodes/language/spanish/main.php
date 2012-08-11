c2f0e83cd968fc528e766adfa5255ca1c4c3baa7
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2012-08-11 12:46:10 +0000

    Removing spaces from "define", which is required for Transifex per DCrusader

--- release/1.05/htdocs/modules/xincodes/language/spanish/main.php
+++ release/1.05/htdocs/modules/xincodes/language/spanish/main.php
@@ -1,34 +1,34 @@
 <?php
 
-	define('_XIN_MF_CODE', 'Código de la invitación');
-	define('_XIN_MF_CODE_DESC', 'Código de la invitación');
+	define('_XIN_MF_CODE','CÃ³digo de la invitaciÃ³n');
+	define('_XIN_MF_CODE_DESC','CÃ³digo de la invitaciÃ³n');
 	
-	define('_XIN_MF_EMAIL', 'Email de tu amigo');
-	define('_XIN_MF_EMAIL_DESC', 'Email del destinatario de la invitación');
+	define('_XIN_MF_EMAIL','Email de tu amigo');
+	define('_XIN_MF_EMAIL_DESC','Email del destinatario de la invitaciÃ³n');
 	
 	// Create Code Form
-	define('_XIN_MF_H1_CREATECODE', 'Crear código de invitación');
-	define('_XIN_MF_P_CREATECODE', 'Introduce el email de tu amigo y se le enviará automáticamente la invitación.');
-	define('_XIN_MF_P_FOOT_CREATECODE', '');
-	define('_XIN_FRM_CREATECODE', 'Introduce el email de tu amigo para que se pueda registrar en la web');
-	define('_XIN_MF_H2_YOUHAVE', 'Te quedan %s invitaciones');
-	define('_XIN_MF_MAXIMUMCODES', '¡Has gastado todas tus invitaciones!');
+	define('_XIN_MF_H1_CREATECODE','Crear cÃ³digo de invitaciÃ³n');
+	define('_XIN_MF_P_CREATECODE','Introduce el email de tu amigo y se le enviarÃ¡ automÃ¡ticamente la invitaciÃ³n.');
+	define('_XIN_MF_P_FOOT_CREATECODE','');
+	define('_XIN_FRM_CREATECODE','Introduce el email de tu amigo para que se pueda registrar en la web');
+	define('_XIN_MF_H2_YOUHAVE','Te quedan %s invitaciones');
+	define('_XIN_MF_MAXIMUMCODES','Â¡Has gastado todas tus invitaciones!');
 	
 	// Email function
-	define('_XIN_MF_ERROROCCURED', 'Un error ocurrió al enviar: %s');
-	define('_XIN_MF_CODECREATEDANDEMAILED', 'Invitación enviada');
+	define('_XIN_MF_ERROROCCURED','Un error ocurriÃ³ al enviar: %s');
+	define('_XIN_MF_CODECREATEDANDEMAILED','InvitaciÃ³n enviada');
 	
 	// Email Subject
-	define('_XINCODES_MSG_SUBJECT', 'Invitación para poder registrarte de %s - %s');
+	define('_XINCODES_MSG_SUBJECT','InvitaciÃ³n para poder registrarte de %s - %s');
 	
 	// Listed codes
-	define('_XIN_MF_H1_YOURCODES', 'Tus invitaciones');
-	define('_XIN_MF_P_YOURCODES', 'Las invitaciones que has enviado se listan a continuación:');
-	define('_XIN_MF_TD_CODE', 'Invitación');
-	define('_XIN_MF_TD_EMAIL', 'Email');
-	define('_XIN_MF_TD_CLAIMED', 'Usada');
-	define('_XIN_MF_TD_FOOT_CODE', '');
-	define('_XIN_MF_TD_FOOT_EMAIL', '');
-	define('_XIN_MF_TD_FOOT_CLAIMED', '');
+	define('_XIN_MF_H1_YOURCODES','Tus invitaciones');
+	define('_XIN_MF_P_YOURCODES','Las invitaciones que has enviado se listan a continuaciÃ³n:');
+	define('_XIN_MF_TD_CODE','InvitaciÃ³n');
+	define('_XIN_MF_TD_EMAIL','Email');
+	define('_XIN_MF_TD_CLAIMED','Usada');
+	define('_XIN_MF_TD_FOOT_CODE','');
+	define('_XIN_MF_TD_FOOT_EMAIL','');
+	define('_XIN_MF_TD_FOOT_CLAIMED','');
 	
 ?>
